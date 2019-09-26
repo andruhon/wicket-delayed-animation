e799a1dfbd24fd35e0701d452c95cbf51f732186
Andrei Kondratev <andrei.kondratev@unimarket.com>
2019-09-26 12:50:41 +1200

    try global suspend

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Considering alternatives to https://wicketinaction.com/2013/02/replace-component
 
 See possible solutions in branches.
 
-This is isolated JS experiment, only have [wicket-ajax-jquery.js](lib/wicket-ajax-jquery.js) and fake response in [response.xml](response.xml)
+This is isolated JS experiment, only have [wicket-ajax-jquery.js](lib/wicket-ajax-jquery.js) and fake responses in [response1.xml](response1.xml) [response2.xml](response2.xml)
 
 [wicket-ajax-jquery.js](lib/wicket-ajax-jquery.js) is unfinished experimental one from incomplete https://github.com/apache/wicket/pull/382
 
--- index.js
+++ index.js
@@ -4,18 +4,19 @@
 
 /**
  * This code is executed before the HTML element is replaced with a new representation.nd add extra classes to index.html
- * see response.xml
+ *see response1.xml and response2.xml
  * @param {String} id
  */
 function myHideFunction(id) {
-  $("#"+id).fadeOut(500, function(){
+  var notify = Wicket.Ajax.Call.suspend();
+  $("#"+id).fadeOut(2500, function(){
     notify();
   });
 }
 
 /**
  * This code is executed after the HTML element is replaced with a new representation.
- * see response.xml
+ * see response1.xml and response2.xml
  * @param {String} id
  */
 function myShowFunction(id) {
@@ -26,7 +27,10 @@ function myShowFunction(id) {
  * Essentially that's approximately what the listener from above pretends to do
  */
 Wicket.Event.add(window, "domready", function(event) {
-  document.getElementById("doSomething").addEventListener("click", function(){
-    Wicket.Ajax.get({u: "/response.xml"});
+  document.getElementById("doSomething1").addEventListener("click", function(){
+    Wicket.Ajax.get({u: "/response1.xml"});
   });
+    document.getElementById("doSomething2").addEventListener("click", function(){
+        Wicket.Ajax.get({u: "/response2.xml"});
+    });
 });
